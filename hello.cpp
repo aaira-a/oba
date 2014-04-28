46ddf9a1262918fb91fb5e7923a34723aef36220
azama <aethelfrith@gmail.com>
2014-04-29 01:44:10 +0800

    hello poc using otm::field. managed to compile & display hello

--- hello.cpp
+++ hello.cpp
@@ -6,7 +6,11 @@
 using namespace std;
 
 int main(){
-	OBATargetMachine::setPeriodicTimer(5,5);
+
+	
+	cout << OBATargetMachine::pulseCounterWordAddress;
+	cout << "\r\n  \r\n";
+	//OBATargetMachine::setPeriodicTimer(5,5);
 	cout << "\r\nHello World!\r\n";
 	cout << "\r\nPress any key to continue...\r\n";
 	cin.get();
