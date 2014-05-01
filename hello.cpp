94faac41fba13638a104fe7766386cda710f9aa8
azam alias <aethelfrith@gmail.com>
2014-05-02 02:20:05 +0800

    managed to get interrupts & hello world from eg

--- hello.cpp
+++ hello.cpp
@@ -5,6 +5,49 @@
 
 using namespace std;
 
+static char * msg; 
+static	char msg1[] = "Hello World!             ";
+static	char msg2[] = "Keyboard Pressed         ";
+static	char msg3[] = "Driving Control          ";
+static	char msg4[] = "                         ";
+static volatile int temp = 100;
+static const unsigned int	periodInMillisecond = 100; 
+
+void periodicHandler (/*OTM::*/Interrupt sig) {
+	ControlPanelInterruptStatusWord *keyId;
+
+	// in function of the signal number, a specific treatment is done
+	switch (sig)
+	{
+	case IT_controlPanel : 
+		// read the Interrupt word 
+		keyId = (ControlPanelInterruptStatusWord *) (OTM::controlPanelInterruptWordAddress);
+		// perform the treatment
+		cout << "Key Id :" << keyId->keyCode << endl;
+		msg = msg2; temp = 10;
+		break;
+	case IT_drivingControls :
+		// to be done	
+		cout << "IT driving" <<endl;
+		msg = msg3; temp = 10;
+		break;
+	case IT_timer :
+		
+		// very simple treatement
+		temp--;
+		// if the time is consumed to display Interrupt message, the default message is set
+		if (temp < 0)
+			msg = msg4;
+
+		cout << ".";cout.flush();
+		break;
+	default :
+	cout << "handler " << sig <<endl;
+	}
+}
+
+
+
 int main(){
 
 	cout << " " << endl;
@@ -48,13 +91,13 @@ int main(){
 	cout << " " << endl;
 
 	//pointer dereference, using unsigned short casting since ori is void pointer
-    cout << *(unsigned short *) OBATargetMachine::ledCommandByteAddress;
+    //cout << *(unsigned short *) OBATargetMachine::ledCommandByteAddress;
 
     cout << " " << endl;
 
     //attempt to write to that
-	unsigned short ledAll = 1;
-	*(unsigned short *) OBATargetMachine::ledCommandByteAddress = ledAll;
+	unsigned short ledAll = 131;
+	//*(unsigned short *) OBATargetMachine::ledCommandByteAddress = ledAll;
 
     cout << *(unsigned short *) OBATargetMachine::ledCommandByteAddress;
 
@@ -66,4 +109,54 @@ int main(){
 	//cin.get();
 	//errorful         *data = (bool)input2;
 	//supposedtowork   *(bool *) data = (bool) input2;
+
+
+    int i=0;
+	msg=msg1;
+	DisplayBufferAddress	 *display;
+
+try {
+// initialisation of the interrupt vector
+
+		OTM::interruptVector[IT_controlPanel] = periodicHandler;
+        OTM::interruptVector[IT_drivingControls] = periodicHandler;
+		OTM::interruptVector[IT_timer] = periodicHandler;
+// set the timer
+
+		OTM::setPeriodicTimer(periodInMillisecond, 0);
+// enable the interrupt
+
+        OTM::enabledInterruptsRegister = allObaRelatedInterrupts;
+
+// keep the display buffer 
+
+		display =  (DisplayBufferAddress*)(OTM::displayBufferAddress);
+		
+
+
+// infinite loop
+
+ for (;;)
+	{
+		
+	
+// write the message to the display
+
+		for (int j=0;j<20;j++)
+			display->displaymessage[j]=msg[j];
+// idle the CPU		
+
+		OTM::idleWait (75);
+	}
+   }
+
+   catch (const char *const message) {
+ //     cout 
+  //    << "Unexpected issue previous to OBA::run():" << endl 
+   //   << "   " << message << endl;
+      // context failure : useless to run OBA.
+      throw;
+   }
+
+
 }
