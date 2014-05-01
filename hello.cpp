fabdc934983147f22152248579684e503f44f208
azama <aethelfrith@gmail.com>
2014-05-02 01:23:09 +0800

    attempt to light led, but ofc crashd

--- hello.cpp
+++ hello.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main(){
 
-	/*
+	cout << " " << endl;
 	cout << "bool:\t\t" << sizeof(bool) << " bytes" << endl;
     cout << "char:\t\t" << sizeof(char) << " bytes" << endl;
     cout << "wchar_t:\t" << sizeof(wchar_t) << " bytes" << endl;
@@ -17,8 +17,9 @@ int main(){
     cout << "float:\t\t" << sizeof(float) << " bytes" << endl;
     cout << "double:\t\t" << sizeof(double) << " bytes" << endl;
     cout << "long double:\t" << sizeof(long double) << " bytes" << endl;
+	cout << "unsigned:\t" << sizeof(unsigned) << " bytes" << endl;
     cout << " " << endl;
-	*/
+	
 	cout << " " << endl;
 
 	cout << "OBATargetMachine::pulseCounterWordAddress \t\t" << OBATargetMachine::pulseCounterWordAddress << endl;
@@ -46,8 +47,23 @@ int main(){
 
 	cout << " " << endl;
 
+	//pointer dereference, using unsigned short casting since ori is void pointer
+    cout << *(unsigned short *) OBATargetMachine::ledCommandByteAddress;
+
+    cout << " " << endl;
+
+    //attempt to write to that
+	unsigned short ledAll = 1;
+	*(unsigned short *) OBATargetMachine::ledCommandByteAddress = ledAll;
+
+    cout << *(unsigned short *) OBATargetMachine::ledCommandByteAddress;
+
+	//cout << &OBATargetMachine::ledCommandByteAddress;
+
 	//OBATargetMachine::setPeriodicTimer(5,5);
-	cout << "\r\nHello World!\r\n";
-	cout << "\r\nPress any key to continue...\r\n";
-	cin.get();
+	//cout << "\r\nHello World!\r\n";
+	//cout << "\r\nPress any key to continue...\r\n";
+	//cin.get();
+	//errorful         *data = (bool)input2;
+	//supposedtowork   *(bool *) data = (bool) input2;
 }
