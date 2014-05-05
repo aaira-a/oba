ec7812ce5d8397055d1f219c2b9622caf7e1167a
azama <aethelfrith@gmail.com>
2014-05-06 02:24:24 +0800

    found one optimized function on the internet

--- RnD_workspace/parity.cpp
+++ RnD_workspace/parity.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
+#include <stdio.h>
 
-int getParityBit(int input) {
-	int output=1;
+using namespace std;
 
-	return output; 
+
+bool getOddParityBit(unsigned int x) {
+	
+	x = ((x >> 1) & 0x55555555) + (x & 0x55555555);
+	x = ((x >> 2) & 0x33333333) + (x & 0x33333333);
+	x = ((x >> 4) & 0x0F0F0F0F) + (x & 0x0F0F0F0F);
+	x = ((x >>  8 ) + x);
+	x = ((x >>  16 ) + x);
+	return !(x&0x01); //return odd parity bit
 }
 
 
-
 int main() {
-	
-	getParityBit(1);
+	//cout << "unsigned:\t" << sizeof(unsigned int) << " bytes" << endl;
 
+	int input;
+	int output;
+
+	for (;;) {
+		cout << "Please input an unsigned int: \t";
+		cin >> input;
+		output = getOddParityBit(input);
+		cout << "The output is: \t" << output << endl;
+	}
 }
